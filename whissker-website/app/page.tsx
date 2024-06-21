821a82dd976eba185cd2de844e8bf2af070536e0
jacton22 <jacton22@icloud.com>
2024-06-21 12:48:20 -0400

    Change title
    Just testing out changing the code a little bit so I know what I'm doing lol

--- whissker-website/app/page.tsx
+++ whissker-website/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center">Welcome to Whissker!</h1>
+      <h1 className="text-4xl font-bold text-center">Whissker</h1>
       <Image src="/whissker-logo.png" width={200} height={200} alt="Whissker Logo" />
       <p className="text-center text-lg">Official website coming soon!</p>
     </main>
