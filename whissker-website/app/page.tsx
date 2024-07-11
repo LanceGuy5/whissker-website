d78536fa20352604cba3be5c65c49e9129862dcd
jacton22 <jacton22@icloud.com>
2024-07-11 22:45:25 +0100

    Update page.tsx

--- whissker-website/app/page.tsx
+++ whissker-website/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         
-        <h1 className="text-4xl font-bold text-center">Whissker</h1>
-        <Image src="/whissker-logo.png" width={200} height={200} alt="Whissker Logo" />
-        <p className="text-center text-lg">Official website coming soon!</p>
+      <h1 className="text-4xl font-bold text-center">Whissker</h1>
+      <Image src="/whissker-logo.png" width={200} height={200} alt="Whissker Logo" />
+      <p className="text-center text-lg">Official website coming soon!</p>
         
     </main>
 
